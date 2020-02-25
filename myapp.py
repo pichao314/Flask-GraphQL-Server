d2c0e235d5d5a56458b62384a6107d0603fa7c65
pcusv <pichao314@gmail.com>
2020-02-25 22:37:31 +0000

    finished

--- myapp.py
+++ myapp.py
@@ -1,21 +1,26 @@
-from ariadne import gql, QueryType, graphql_sync, make_executable_schema,MutationType
+from ariadne import gql, QueryType, ObjectType, graphql_sync, make_executable_schema, load_schema_from_path
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, request, jsonify
 
-type_defs = gql("""#the gql validate schema and raises error
-    type Query {
-        hello: String!
-        test(name: String): String!
-        all: [String]!
-    }
-    type Mutation{
-        add(name: String!): String!
-    }
-""")
 
-users = ["Unknown"]
+type_defs = load_schema_from_path("./schema.graphql")
 
-query = QueryType()
+query = ObjectType("Query")
+
+st = [
+    {
+        'id': 1,
+        'name': "chao"
+    }
+]
+
+cl = [
+    {
+        'id': 273,
+        'name': "Distributed System",
+        'students': []
+    }
+]
 
 
 @query.field("hello")
@@ -25,22 +30,90 @@ def resolve_hello(_, info):
     return "Hello, %s!" % user_agent
 
 
-@query.field("test")
-def resolve_test(*_, name=users[-1]):
-    return "Hello, %s" % name
+@query.field("students")
+def resolve_students(obj, info):
+    return st
 
-@query.field("all")
-def resolve_all(*_):
-    return users
 
-mutation = MutationType()
+@query.field("student")
+def resolve_student(obj, info, id):
+    for each in st:
+        if each['id'] == id:
+            return each
+    return None
+
+
+@query.field("classes")
+def resolve_classes(obj, info):
+    return cl
+
+
+@query.field("class")
+def resolve_classes(obj, info, id):
+    for each in cl:
+        if each['id'] == id:
+            return each
+    return None
+
+
+mutation = ObjectType("Mutation")
+
+
+@mutation.field("student")
+def resolve_m_student(obj, info, name):
+    st.append(
+        {
+            'id': len(st)+1,
+            'name': name
+        }
+    )
+    return True
+
+
+@mutation.field("class")
+def resolve_m_class(obj, info, id, name):
+    for each in cl:
+        if each["id"] == id:
+            return False
+    cl.append(
+        {
+            'id': id,
+            'name': name,
+            'students': []
+        }
+    )
+    return True
+
 
 @mutation.field("add")
-def resolve_add(_,info,name):
-    users.append(name)
-    return users[-1]
+def resolve_add(obj, info, sid, cid):
+    for s in st:
+        if s['id'] == sid:
+            for c in cl:
+                if c['id'] == cid:
+                    for cs in c['students']:
+                        if cs['id'] == sid:
+                            return False
+                    c['students'].append(s)
+                    return True
+            return False
+    return False
 
-schema = make_executable_schema(type_defs, query, mutation)
+
+student = ObjectType("Student")
+
+
+@student.field("id")
+def resolve_students_id(obj, info):
+    return obj['id']
+
+
+@student.field("name")
+def resolve_students_name(obj, info):
+    return obj['name']
+
+
+schema = make_executable_schema(type_defs, query, mutation, student)
 
 app = Flask(__name__)
 
